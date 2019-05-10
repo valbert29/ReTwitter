fbdbf9594b33e8071455292373f9185e80ed81f0
IrinaSkripka99 <irinaskripka99@gmail.com>
2019-05-10 17:03:45 +0300

    прикручена верста и создана страница твитов от всех пользователей

--- account.php
+++ account.php
@@ -45,26 +45,26 @@
 <?php
 require_once("includes/connection.php");
 session_start();
-$login=$_SESSION['session_login'];
+
 if (!empty($_GET["login"])) {
+    $login=$_SESSION['session_login'];
     $login_add = htmlspecialchars($_GET['login']);
     if($login!==$login_add){
-        $user=$_SESSION['session_login'];
-
-
-        $findUser = pg_query($con, "SELECT id FROM user_t WHERE login='" . $user . "'");
+        $findUser = pg_query($con, "SELECT id FROM user_t WHERE login='" . $login . "'");
         $findUser = pg_fetch_array($findUser, 0, PGSQL_NUM);
         $findUser = $findUser[0];
 
-        $findLogin = pg_query($con, "SELECT id FROM user_t WHERE login='" . $login . "'");
+        $findLogin = pg_query($con, "SELECT id FROM user_t WHERE login='" . $login_add . "'");
         $findLogin = pg_fetch_array($findLogin, 0, PGSQL_NUM);
         $findLogin=$findLogin[0];
+        print $findLogin;
         $search_follower=pg_query($con,"SELECT * FROM followers where user_id='$findLogin' AND follower_id='$findUser'");
+        print pg_num_rows($search_follower);
         if(pg_num_rows($search_follower)==0) {
             print '<script type="text/javascript">
           var div=document.getElementsByClassName("owner");
           var para = document.createElement("div");
-          para.innerHTML=\'<button class="btn btn-danger btn-round" onclick="addFollower()"><i class="nc-icon nc-tile-56"></i><a style="color:white" href="account.php?add_follow_user=' . $login . '">Подписаться</a></button>\';
+          para.innerHTML=\'<button class="btn btn-danger btn-round" onclick="addFollower()"><i class="nc-icon nc-tile-56"></i><a style="color:white" href="account.php?add_follow_user=' . $login_add . '">Подписаться</a></button>\';
           var node = document.createTextNode("");
           para.appendChild(node);
           div[0].appendChild(para);
@@ -81,6 +81,8 @@ if (!empty($_GET["login"])) {
         }
     }
 
+}else{
+    $login_add=$_SESSION['session_login'];
 }
 if (!empty($_GET["add_follow_user"])) {
     $follower = $_SESSION['session_login'];
@@ -98,7 +100,7 @@ if (!empty($_GET["add_follow_user"])) {
         "INSERT into followers(user_id, follower_id) VALUES('$findUser','$findFollower'); ");
 }
 
-$result = pg_query($con, "SELECT * FROM user_t WHERE login='" . $login . "'");
+$result = pg_query($con, "SELECT * FROM user_t WHERE login='" . $login_add . "'");
 $arr = pg_fetch_array($result, 0, PGSQL_NUM);
 $user_id = $arr[0];
 $login = $arr[1];
